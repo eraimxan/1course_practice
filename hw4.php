529ee449c837233d813816f6abda121448715461
eraimxan <155010938+eraimxan@users.noreply.github.com>
2025-06-15 20:40:09 +0500

    Add files via upload

---
+++ hw4.php
@@ -0,0 +1,26 @@
+<?php
+// 1. Информация о студенте
+$studentName = "Алина";
+$studentAge = 20;
+$studentGPA = 3.8;
+
+// 2. Информация об автомобиле
+$carBrand = "Lexus";
+$carYear = 2018;
+$carMileage = 75000; // в километрах
+
+// 3. Информация о покупке в интернет-магазине
+$productName = "Беспроводные наушники";
+$productQuantity = 2;
+$productPricePerUnit = 15990;
+
+// 4. Личные данные пользователя
+$userName = "Ерасыл";
+$userEmail = "erasyl@example.com";
+$userBirthDate = "2005-03-15";
+
+// 5. Информация о продукте в интернет-магазине
+$itemTitle = "Умные часы";
+$itemCategory = "Электроника";
+$itemCountry = "Тайвань";
+?>
